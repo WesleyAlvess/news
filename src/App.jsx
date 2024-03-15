90bc83d89f9710448a1ef5d88c3759e871a3c7d1
WesleyAlvess <walvespereira96@gmail.com>
2024-03-15 08:45:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import components
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
+
+function App() {
+
+    return (
+        <div className=" w-full h-screen bg-[#E5E7EB]">
+            <Home />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
